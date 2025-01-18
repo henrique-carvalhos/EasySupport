07292c461dc62cac7ba4d8fece1b707fc8abfd89
Henrique carvalho <henriqueart7@gmail.com>
2025-01-18 10:25:21 -0300

    config route login-layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginLayoutComponent } from './components/login-layout/login-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginLayoutComponent
+    }
+];
