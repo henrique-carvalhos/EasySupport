e3a0bb10afd50d73f909d5a15b662013d5ba30a3
Henrique carvalho <henriqueart7@gmail.com>
2025-01-18 13:00:37 -0300

    import EsButtonComponent

--- src/app/components/login-layout/login-layout.component.ts
+++ src/app/components/login-layout/login-layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { EsButtonComponent } from "../../shared/components/es-button/es-button.component";
 
 @Component({
   selector: 'app-login-layout',
-  imports: [],
+  imports: [EsButtonComponent],
   templateUrl: './login-layout.component.html',
   styleUrl: './login-layout.component.scss'
 })
