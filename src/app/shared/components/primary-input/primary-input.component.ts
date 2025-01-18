32088d06b6df377a4e0840b20535953803226ff5
Henrique carvalho <henriqueart7@gmail.com>
2025-01-18 13:03:39 -0300

    add primary-input component

---
+++ src/app/shared/components/primary-input/primary-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-input',
+  imports: [],
+  templateUrl: './primary-input.component.html',
+  styleUrl: './primary-input.component.scss'
+})
+export class PrimaryInputComponent {
+
+}
