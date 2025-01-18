ec7933a57fbff08f5f1e6cf18a808b3b08171848
Henrique carvalho <henriqueart7@gmail.com>
2025-01-18 15:59:57 -0300

    import ReactiveFormsModule

--- src/app/components/login-layout/login-layout.component.ts
+++ src/app/components/login-layout/login-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { EsButtonComponent } from "../../shared/components/es-button/es-button.component";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-layout',
-  imports: [EsButtonComponent],
+  imports: [EsButtonComponent, ReactiveFormsModule],
   templateUrl: './login-layout.component.html',
   styleUrl: './login-layout.component.scss'
 })
