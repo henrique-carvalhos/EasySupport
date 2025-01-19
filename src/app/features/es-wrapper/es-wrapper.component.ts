af3a3485d603ae5034ed6e48f5aaaf7a2f8b2387
Henrique carvalho <henriqueart7@gmail.com>
2025-01-19 19:04:57 -0300

    feat: building the es-wrapper

--- src/app/features/es-wrapper/es-wrapper.component.ts
+++ src/app/features/es-wrapper/es-wrapper.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { EsHeaderComponent } from "../es-header/es-header.component";
 
 @Component({
   selector: 'app-es-wrapper',
-  imports: [],
+  imports: [EsHeaderComponent],
   templateUrl: './es-wrapper.component.html',
   styleUrl: './es-wrapper.component.scss'
 })
