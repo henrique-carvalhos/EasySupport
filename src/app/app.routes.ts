90410305ed779a2f9605c50bc68ffb766732b79c
Henrique carvalho <henriqueart7@gmail.com>
2025-01-19 18:58:07 -0300

    feat: config router tickets-user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ export const routes: Routes = [
         component: LoginComponent
     },
     {
-        path: 'tickets-users',
+        path: 'tickets-user',
         component: ListTicketsUserComponent
     }
 ];
