df6b7bc1fa4a0979f3a9061debc257a53991b0ce
Henrique carvalho <henriqueart7@gmail.com>
2025-01-19 12:42:22 -0300

    config route list-tickets component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { ListTicketsUserComponent } from './pages/list-tickets-user/list-tickets-user.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LoginComponent
+    },
+    {
+        path: 'tickets-users',
+        component: ListTicketsUserComponent
     }
 ];
