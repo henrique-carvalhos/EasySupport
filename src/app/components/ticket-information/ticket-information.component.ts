b0a72138bdcea358e94befea16d51f08c6164640
Henrique carvalho <henriqueart7@gmail.com>
2025-01-20 20:54:42 -0300

    feat: add EsButtonComponent in ticket-information component

--- src/app/components/ticket-information/ticket-information.component.ts
+++ src/app/components/ticket-information/ticket-information.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { EsButtonComponent } from "../../shared/components/es-button/es-button.component";
 
 @Component({
   selector: 'app-ticket-information',
-  imports: [],
+  imports: [EsButtonComponent],
   templateUrl: './ticket-information.component.html',
   styleUrl: './ticket-information.component.scss'
 })
