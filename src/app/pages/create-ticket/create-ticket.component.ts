3aacd1419937d860c3c41dddd0694d2c06c34f57
Henrique carvalho <henriqueart7@gmail.com>
2025-01-20 19:30:33 -0300

    feat: add create-ticket component

---
+++ src/app/pages/create-ticket/create-ticket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-ticket',
+  imports: [],
+  templateUrl: './create-ticket.component.html',
+  styleUrl: './create-ticket.component.scss'
+})
+export class CreateTicketComponent {
+
+}
