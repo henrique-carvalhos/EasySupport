bca2773da7420fb5467a24e6dd3338de349083ab
Henrique carvalho <henriqueart7@gmail.com>
2025-01-20 20:52:34 -0300

    feat: add EsWrapperComponent and TicketInformationComponent in ticket component

--- src/app/pages/ticket/ticket.component.ts
+++ src/app/pages/ticket/ticket.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { EsWrapperComponent } from "../../features/es-wrapper/es-wrapper.component";
+import { TicketInformationComponent } from "../../components/ticket-information/ticket-information.component";
 
 @Component({
   selector: 'app-ticket',
-  imports: [],
+  imports: [EsWrapperComponent, TicketInformationComponent],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.scss'
 })
