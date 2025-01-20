86166a7dec3790ad514c17f65ba9c14686e8098c
Henrique carvalho <henriqueart7@gmail.com>
2025-01-20 19:44:58 -0300

    feat: add ticket-information component

---
+++ src/app/components/ticket-information/ticket-information.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-information',
+  imports: [],
+  templateUrl: './ticket-information.component.html',
+  styleUrl: './ticket-information.component.scss'
+})
+export class TicketInformationComponent {
+
+}
