b2a8bac225738aae35cf9331d97e03872d06516b
Henrique carvalho <henriqueart7@gmail.com>
2025-01-21 21:19:03 -0300

    feat: add EsWrapperComponent in create-ticket

--- src/app/pages/create-ticket/create-ticket.component.ts
+++ src/app/pages/create-ticket/create-ticket.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { EsWrapperComponent } from "../../features/es-wrapper/es-wrapper.component";
+import { EsButtonComponent } from "../../shared/components/es-button/es-button.component";
 
 @Component({
   selector: 'app-create-ticket',
-  imports: [EsWrapperComponent],
+  imports: [EsWrapperComponent, EsButtonComponent],
   templateUrl: './create-ticket.component.html',
   styleUrl: './create-ticket.component.scss'
 })
