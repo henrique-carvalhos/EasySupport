7a20a106756336b3fcc0e0f3e9dba80aa1d276f6
Henrique carvalho <henriqueart7@gmail.com>
2025-01-28 22:17:09 -0300

    feat: add dynamic class in es-button component

--- src/app/shared/components/es-button/es-button.component.ts
+++ src/app/shared/components/es-button/es-button.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-es-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './es-button.component.html',
   styleUrl: './es-button.component.scss'
 })
 export class EsButtonComponent {
   @Input() text: string = '';
+  tamanho!: string;
+  cor!: string;
+
+  constructor(private router: Router){
+    const url = this.router.url;
+
+    if(url.includes('/tickets-user')){
+      this.tamanho = '-small';
+      this.cor = '';
+    }
+  }
 }
