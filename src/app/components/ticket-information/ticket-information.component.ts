8de4075b6f8c13001f623f7a3f361c611b2d1da9
Henrique carvalho <henriqueart7@gmail.com>
2025-01-29 22:14:26 -0300

    feat: add custom tollbar quill

--- src/app/components/ticket-information/ticket-information.component.ts
+++ src/app/components/ticket-information/ticket-information.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { EsButtonComponent } from "../../shared/components/es-button/es-button.component";
+import { QuillModule } from 'ngx-quill';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket-information',
-  imports: [EsButtonComponent],
+  imports: [EsButtonComponent,  QuillModule, ReactiveFormsModule],
   templateUrl: './ticket-information.component.html',
   styleUrl: './ticket-information.component.scss'
 })
 export class TicketInformationComponent {
+conteudo = new FormControl('');
 
+  customModules = {
+    toolbar: [
+      ['bold', 'italic', 'underline'], // Negrito, Itálico, Sublinhado
+      [{ list: 'ordered' }, { list: 'bullet' }], // Listas numeradas e com marcadores
+      [{ header: [1, 2, 3, false] }], // Tamanhos de título
+      ['blockquote', 'code-block'], // Bloco de citação e código
+      [{ align: [] }], // Alinhamento de texto
+      [{ color: [] }, { background: [] }], // Cores de texto e fundo
+      ['clean'] // Remover formatação
+    ]
+  };
 }
