7eb0fb2b89a6ad16814ed11dbaa55be344d00ae6
Henrique carvalho <henriqueart7@gmail.com>
2025-01-30 21:11:28 -0300

    feat: add logo component in login-layout component

--- src/app/components/login-layout/login-layout.component.ts
+++ src/app/components/login-layout/login-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { EsButtonComponent } from "../../shared/components/es-button/es-button.component";
 import { ReactiveFormsModule } from '@angular/forms';
+import { LogoComponent } from "../../shared/components/logo/logo.component";
 
 @Component({
   selector: 'app-login-layout',
-  imports: [EsButtonComponent, ReactiveFormsModule],
+  imports: [EsButtonComponent, ReactiveFormsModule, LogoComponent],
   templateUrl: './login-layout.component.html',
   styleUrl: './login-layout.component.scss'
 })
