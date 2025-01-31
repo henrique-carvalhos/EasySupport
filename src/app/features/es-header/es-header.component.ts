fbcc5c04ddc3f64a2768e8ce1303559f06a1185e
Henrique carvalho <henriqueart7@gmail.com>
2025-01-30 21:11:54 -0300

    feat: add logo component in es-header component

--- src/app/features/es-header/es-header.component.ts
+++ src/app/features/es-header/es-header.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LoginComponent } from "../../pages/login/login.component";
+import { LogoComponent } from "../../shared/components/logo/logo.component";
 
 @Component({
   selector: 'app-es-header',
-  imports: [CommonModule],
+  imports: [CommonModule, LogoComponent],
   templateUrl: './es-header.component.html',
   styleUrl: './es-header.component.scss'
 })
